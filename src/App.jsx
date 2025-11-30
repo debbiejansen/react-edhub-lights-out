fcf1f3695b22d1075e796929465814e291e10848
debbiejansen <debbiejansen94@hotmail.com>
2025-11-30 14:27:25 +0100

    hereby pushing new code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [isOn, setIsOn] = useState('false');
+    console.log("Light is currently: ", isOn);
+    function toggleLight() {
+        setIsOn(!isOn);
+    }
     return (
-        <main className="off">
+        <main className={isOn ? "on" : "off"}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button
+                    type="button"
+                    className={isOn ? "on" : "off"}
+                    onClick={toggleLight}
+                >
+                    {isOn
+                        ? "Turn off"
+                        : "Turn on"
+                    }
+                </button>
             </section>
         </main>
     );
